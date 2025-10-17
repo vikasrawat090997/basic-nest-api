b6c262f6d9b2f4b910eb22b67bc94d45d9279151
Vikas Kumar <vikaskumar@aurosociety.org>
2025-10-17 10:53:16 +0530

    Test auto deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Sir, this is your first NestJS API!';
+    return 'Hello World!';
   }
 }
